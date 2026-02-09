80cfed924436172f711374ea73a80c305081c3ff
VuLkAn911 <donkikon828@gmail.com>
2026-02-09 20:13:53 +0500

    A

---
+++ Subject2.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// sub 1
+	schooling := 11
+	fmt.Println(schooling)
+
+	schooling = 12
+	fmt.Println(schooling)
+
+	// sub 2
+
+	name := "Vladislav"
+	fmt.Println(name)
+	name = "Abylay"
+	fmt.Println(name)
+
+	// sub 3
+
+	steps := 0
+
+	fmt.Println(steps)
+
+	steps = 2000
+
+	fmt.Println(steps)
+
+	fmt.Printf("Хорошая работа! Вы уже на пути к своей ежедневной цели: %d Шагов", steps)
+
+	// sub 4
+
+	largeNumber := 5000001
+
+	fmt.Println(largeNumber)
+
+	// sub 5
+	const breakTime = 15
+	fmt.Println(breakTime)
+
+	breaktime = 20
+
+	// Константу нельзя изменять
+
+}
